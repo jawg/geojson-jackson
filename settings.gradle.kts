8dd48b66dad805b98c5eb08885628b1aae4a0239
clementgayvallet <clement@jawg.io>
2026-06-09 11:30:34 +0200

    build: migrate Gradle dependencies to a Version Catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,5 @@
 rootProject.name = "geojson-jackson"
 
-pluginManagement {
-  plugins {
-    kotlin("jvm") version "${extra["version.kotlin"]}"
-    id("com.github.ben-manes.versions") version "${extra["version.versions.plugin"]}"
-    id("com.vanniktech.maven.publish") version "${extra["version.maven-publish.plugin"]}"
-  }
-}
-
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
